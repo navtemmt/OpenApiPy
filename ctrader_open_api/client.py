ebf33a2b8ddae875f6a8edb463d2bd2940ce8575
navtemmt <83179960+navtemmt@users.noreply.github.com>
2026-04-18 16:37:18 +0800

    Refactor Client constructor and response handling

--- ctrader_open_api/client.py
+++ ctrader_open_api/client.py
@@ -6,13 +6,33 @@ from ctrader_open_api.protobuf import Protobuf
 from ctrader_open_api.factory import Factory
 from twisted.internet import reactor, defer
 
+
 class Client(ClientService):
-    def __init__(self, host, port, protocol, retryPolicy=None, clock=None, prepareConnection=None, numberOfMessagesToSendPerSecond=5):
+    def __init__(
+        self,
+        host,
+        port,
+        protocol,
+        retryPolicy=None,
+        clock=None,
+        prepareConnection=None,
+        numberOfMessagesToSendPerSecond=5,
+        defaultResponseTimeoutInSeconds=30,
+    ):
         self._runningReactor = reactor
         self.numberOfMessagesToSendPerSecond = numberOfMessagesToSendPerSecond
+        self.defaultResponseTimeoutInSeconds = defaultResponseTimeoutInSeconds
+
         endpoint = clientFromString(self._runningReactor, f"ssl:{host}:{port}")
         factory = Factory.forProtocol(protocol, client=self)
-        super().__init__(endpoint, factory, retryPolicy=retryPolicy, clock=clock, prepareConnection=prepareConnection)
+        super().__init__(
+            endpoint,
+            factory,
+            retryPolicy=retryPolicy,
+            clock=clock,
+            prepareConnection=prepareConnection,
+        )
+
         self._events = dict()
         self._responseDeferreds = dict()
         self.isConnected = False
@@ -40,23 +60,47 @@ class Client(ClientService):
     def _received(self, message):
         if hasattr(self, "_messageReceivedCallback"):
             self._messageReceivedCallback(self, message)
-        if (message.clientMsgId is not None and message.clientMsgId in self._responseDeferreds):
+
+        if message.clientMsgId is not None and message.clientMsgId in self._responseDeferreds:
             responseDeferred = self._responseDeferreds[message.clientMsgId]
-            self._responseDeferreds.pop(message.clientMsgId)
+            self._responseDeferreds.pop(message.clientMsgId, None)
             responseDeferred.callback(message)
 
-    def send(self, message, clientMsgId=None, responseTimeoutInSeconds=5, **params):
+    def send(self, message, clientMsgId=None, responseTimeoutInSeconds=None, **params):
         if type(message) in [str, int]:
             message = Protobuf.get(message, **params)
+
+        if responseTimeoutInSeconds is None:
+            responseTimeoutInSeconds = self.defaultResponseTimeoutInSeconds
+
         responseDeferred = defer.Deferred(self._cancelMessageDiferred)
+
         if clientMsgId is None:
             clientMsgId = str(id(responseDeferred))
+
         if clientMsgId is not None:
             self._responseDeferreds[clientMsgId] = responseDeferred
-        responseDeferred.addErrback(lambda failure: self._onResponseFailure(failure, clientMsgId))
-        responseDeferred.addTimeout(responseTimeoutInSeconds, self._runningReactor)
-        protocolDiferred = self.whenConnected(failAfterFailures=1)       
-        protocolDiferred.addCallbacks(lambda protocol: protocol.send(message, clientMsgId=clientMsgId, isCanceled=lambda: clientMsgId not in self._responseDeferreds), responseDeferred.errback)
+
+        responseDeferred.addErrback(
+            lambda failure: self._onResponseFailure(failure, clientMsgId)
+        )
+
+        if responseTimeoutInSeconds and responseTimeoutInSeconds > 0:
+            responseDeferred.addTimeout(
+                responseTimeoutInSeconds,
+                self._runningReactor,
+            )
+
+        protocolDiferred = self.whenConnected(failAfterFailures=1)
+        protocolDiferred.addCallbacks(
+            lambda protocol: protocol.send(
+                message,
+                clientMsgId=clientMsgId,
+                isCanceled=lambda: clientMsgId not in self._responseDeferreds,
+            ),
+            responseDeferred.errback,
+        )
+
         return responseDeferred
 
     def setConnectedCallback(self, callback):
@@ -69,11 +113,11 @@ class Client(ClientService):
         self._messageReceivedCallback = callback
 
     def _onResponseFailure(self, failure, msgId):
-        if (msgId is not None and msgId in self._responseDeferreds):
-            self._responseDeferreds.pop(msgId)
+        if msgId is not None and msgId in self._responseDeferreds:
+            self._responseDeferreds.pop(msgId, None)
         return failure
 
     def _cancelMessageDiferred(self, deferred):
         deferredIdString = str(id(deferred))
-        if (deferredIdString in self._responseDeferreds):
-            self._responseDeferreds.pop(deferredIdString)
+        if deferredIdString in self._responseDeferreds:
+            self._responseDeferreds.pop(deferredIdString, None)
