904317186e3a7b6a600a0131f9978731de51f9a3
navtemmt <83179960+navtemmt@users.noreply.github.com>
2026-09-10 12:07:51 +0800

    Update imports in trade_processor.py for compatibility

--- trade_processor.py
+++ trade_processor.py
@@ -2,6 +2,7 @@
 
 from refactored_trade_processor.common import *
 from refactored_trade_processor.risk import *
+from refactored_trade_processor.risk import _enforce_max_risk_on_fill
 from refactored_trade_processor.helpers import *
 from refactored_trade_processor.destination_recovery import *
 from refactored_trade_processor.routing import *
@@ -11,6 +12,3 @@ from refactored_trade_processor.handlers_open import *
 from refactored_trade_processor.handlers_pending import *
 from refactored_trade_processor.handlers_modify_close import *
 from refactored_trade_processor.processor import *
-
-# Explicitly re-export underscore-prefixed legacy API
-from refactored_trade_processor.risk import _enforce_max_risk_on_fill
