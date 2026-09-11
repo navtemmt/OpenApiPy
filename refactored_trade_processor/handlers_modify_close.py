90c518f4b9af4e1b28756803d56941790c77cf00
navtemmt <83179960+navtemmt@users.noreply.github.com>
2026-09-11 15:24:05 +0800

    Refactor imports in handlers_modify_close.py
    Refactor import statements for better readability.

--- refactored_trade_processor/handlers_modify_close.py
+++ refactored_trade_processor/handlers_modify_close.py
@@ -2,18 +2,46 @@ import time
 from threading import Lock
 
 from app_state import (
-    logger, PENDING_SLTP, MASTER_OPEN_LOTS, MASTER_CLOSED_LOTS,
-    alert_trade_failure, alert_trade_warning, alert_trade_info,
+    logger,
+    PENDING_SLTP,
+    MASTER_OPEN_LOTS,
+    MASTER_CLOSED_LOTS,
+    alert_trade_failure,
+    alert_trade_warning,
+    alert_trade_info,
 )
-from trade_executor import (copy_open_to_account, copy_pending_to_account, transition_pending_to_market)
+
+from trade_executor import (
+    copy_open_to_account,
+    copy_pending_to_account,
+    transition_pending_to_market,
+)
+
 from symbol_mapper import SymbolMapper
 
 from .common import *
+from .common import (
+    _to_int,
+    _to_float,
+    _to_float_or_none,
+    _MASTER_LOTS_LOCK,
+)
+
 from .risk import *
+
 from .helpers import *
+
 from .routing import *
+from .routing import (
+    _get_target_account_contexts,
+)
+
 from .sltp_repair import *
-from .destination_recovery import recover_missing_destination
+
+from .destination_recovery import (
+    recover_missing_destination,
+)
+
 
 def handle_modify_event(
     data,
